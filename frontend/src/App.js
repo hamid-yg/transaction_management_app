6c7324f3beb594653780aa6b42c5d70a9c22da69
Blacky <hamidou.tessilimi@epitech.eu>
2022-02-19 02:05:37 +0100

    add: Transactions table

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,51 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import APIProvider from './helpers/APIProvider';
+import APIProvider from './helpers/APIProvider';
 import Transactions from './components/Transactions';
 import AddTransaction from './components/AddTransaction';
 import EditTransaction from './components/EditTransaction';
 
 function App() {
-	const transactionsData = [
-		{ id: 1, amount: 30, description: 'floppydiskette' },
-		{ id: 2, amount: 20, description: 'siliconeidolon' },
-		{ id: 3, amount: 10, description: 'benisphere' },
-	]
 
-	const initial = { id: null, amount: 0, description: '' }
-	const [ transactions, setTransactions ] = useState(transactionsData)
+	useEffect(() => {
+    async function getTransactions() {
+      const result = await APIProvider.getAll()
+			setTransactions(result.data)
+    }
+    getTransactions()
+  }, [])
+
+	const initial = { id: 0, amount: 0, description: '', created_at: "", updated_at: ""}
+	const [ transactions, setTransactions ] = useState(initial)
 	const [ currentTransaction, setCurrentTransaction ] = useState(initial)
 	const [ editing, setEditing ] = useState(false)
 
-	const addTransaction = transaction => {
-		transaction.id = transactions.length + 1;
-		setTransactions([ ...transactions, transaction]);
+	const addTransaction = async (transaction) => {
+		const result = await APIProvider.create(transaction)
+		console.log(result)
+		setTransactions([ ...transactions, result.data])
 	}
 
 	const editTransaction = transaction => {
 		setEditing(true)
-		setCurrentTransaction({ id: transaction.id, amount: transaction.amount, description: transaction.description });
+		setCurrentTransaction({ id: transaction._id, amount: transaction.amount, description: transaction.description, created_at: transaction.created_at, updated_at: transaction.updated_at });
 	}
 
-	const updateTransaction = (id, updatedTransaction) => {
+	const updateTransaction = async (id, updatedTransaction) => {
+		const result = await APIProvider.update(id, updatedTransaction)
+		setTransactions(transactions.map(transaction => (transaction._id === id ? updatedTransaction : transaction)))
 		setEditing(false)
-		setTransactions(transactions.map(transaction => (transaction.id === id ? updatedTransaction : transaction)))
 	}
 
-	const deleteTransaction = id => {
-		setEditing(false)
-		setTransactions(transactions.filter(transaction => transaction.id !== id));
+	const deleteTransaction = async (id) => {
+		const result = await APIProvider.delete(id)
+		setTransactions(transactions.filter(transaction => transaction._id !== id));
 	}
 
   return (
 		<div className="container mt-5">
 			<h1 className='text-primary'>Transaction Management App</h1>
 			<div className="row mt-5">
-				<div className="col-md-4 col-sm-6 mb-5">
+				<div className="col-md-3 col-sm-6 mb-5">
 					{editing ? (
 						<>
-							<h2>Edit transaction</h2>
+							<h2 className='text-primary'>Edit transaction</h2>
 							<EditTransaction
 								editing={editing}
 								setEditing={setEditing}
@@ -55,13 +60,13 @@ function App() {
 						</>
 					) : (
 						<>
-							<h2>Create new transaction</h2>
+							<h2 className='text-primary'>Create new transaction</h2>
 							<AddTransaction addTransaction={addTransaction}/>
 						</>
 					)}
 				</div>
-				<div className="col-md-8 col-sm-6">
-					<h2>View all transactions</h2>
+				<div className="col-md-9 col-sm-6">
+					<h2 className='text-primary'>View all transactions</h2>
 					<Transactions transactions={transactions} updateTransaction={editTransaction} deleteTransaction={deleteTransaction} />
 				</div>
 			</div>
--- frontend/src/components/Transactions.js
+++ frontend/src/components/Transactions.js
@@ -1,38 +1,42 @@
 import React from 'react';
 
-const Transactions = props => (
-  <table className="table table-striped table-bordered text-center">
-    <thead>
-      <tr>
-        <th>Amount</th>
-        <th>Description</th>
-        <th>Created At</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
-    <tbody>
-      {props.transactions.length > 0 ? (props.transactions.map(transaction => (
-        <tr key={transaction.id}>
-          <td>{transaction.amount}</td>
-          <td>{transaction.description}</td>
-          <td>Date</td>
-          <td>
-            <button onClick={() => props.updateTransaction(transaction)} className="btn btn-primary">
-              Edit
-            </button>
-            <button onClick={() => props.deleteTransaction(transaction.id)} className="btn btn-danger">
-              Delete
-            </button>
-          </td>
-        </tr>
-        ))
-      ) : (
+const Transactions = props => {
+  return (
+    <table className="table table-striped table-bordered text-center">
+      <thead>
         <tr>
-          <td colSpan={4}>No transactions</td>
+          <th>Amount</th>
+          <th>Description</th>
+          <th>Created At</th>
+          <th>Updated At</th>
+          <th>Actions</th>
         </tr>
-      )}
-    </tbody>
-  </table>
-);
+      </thead>
+      <tbody>
+        {props.transactions.length > 0 ? (props.transactions.map((transaction, id) => (
+          <tr key={id}>
+            <td>{transaction.amount}</td>
+            <td>{transaction.description}</td>
+            <td>{new Date(transaction.created_at).getFullYear() + '-' + (new Date(transaction.created_at).getMonth() + 1) + '-' + new Date(transaction.created_at).getDate() + ' ' + new Date(transaction.created_at).getHours() + ':' + new Date(transaction.created_at).getMinutes()}</td>
+            <td>{new Date(transaction.updated_at).getFullYear() + '-' + (new Date(transaction.updated_at).getMonth() + 1) + '-' + new Date(transaction.updated_at).getDate() + ' ' + new Date(transaction.updated_at).getHours() + ':' + new Date(transaction.updated_at).getMinutes()}</td>
+            <td>
+              <button onClick={() => props.updateTransaction(transaction)} className="btn btn-primary">
+                Edit
+              </button>
+              <button onClick={() => props.deleteTransaction(transaction._id)} className="btn btn-danger">
+                Delete
+              </button>
+            </td>
+          </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan={5}>No transactions</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  )
+};
 
 export default Transactions;
